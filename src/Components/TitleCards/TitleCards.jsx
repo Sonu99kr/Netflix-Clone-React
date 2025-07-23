fb0315ae07b592facb32ca81caa31a9de362e76d
Sonu99kr <krsonu.work@gmail.com>
2025-07-23 12:35:11 +0530

    home page added

--- src/Components/TitleCards/TitleCards.jsx
+++ src/Components/TitleCards/TitleCards.jsx
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import "./TitleCards.css";
+import cards_data from "../../assets/cards/Cards_data";
 
-const TitleCards = () => {
-  return <div className="titlecards"> TitleCards</div>;
+const TitleCards = ({ title, category }) => {
+  const cardsRef = useRef();
+
+  const hadnleWheel = (event) => {
+    event.preventDefault();
+    cardsRef.current.scrollLeft += event.deltaY;
+  };
+  useEffect(() => {
+    cardsRef.current.addEventListener("wheel", hadnleWheel);
+  }, []);
+  return (
+    <div className="title-cards">
+      <h2>{title ? title : "Popular On Netflix"}</h2>
+      <div className="card-list" ref={cardsRef}>
+        {cards_data.map((card, index) => {
+          return (
+            <div className="card" key={index}>
+              <img src={card.image} alt="" />
+              <p>{card.name}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default TitleCards;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,45 @@
 import React from "react";
 import "./Home.css";
 import Navbar from "../../Components/Navbar/Navbar";
+import hero_banner from "../../assets/hero_banner.jpg";
+import hero_title from "../../assets/hero_title.png";
+import play_icon from "../../assets/play_icon.png";
+import info_icon from "../../assets/info_icon.png";
+import TitleCards from "../../Components/TitleCards/TitleCards";
 
 const Home = () => {
   return (
     <div className="Home">
       <Navbar />
+      <div className="hero">
+        <img src={hero_banner} alt="" className="banner-img" />
+        <div className="hero-caption">
+          <img src={hero_title} alt="" className="caption-img" />
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui,
+            asperiores! Neque accusantium tenetur architecto alias vel ad
+            asperiores reiciendis eum quasi cum. Impedit rem recusandae
+            similique eos facilis molestiae quis.
+          </p>
+          <div className="hero-btns">
+            <button className="btn">
+              <img src={play_icon} alt="" />
+              Play
+            </button>
+            <button className="btn dark-btn">
+              <img src={info_icon} alt="" />
+              More Info
+            </button>
+          </div>
+          <TitleCards />
+        </div>
+      </div>
+      <div className="more-cards">
+        <TitleCards title={"BlockBuster Movies"} />
+        <TitleCards title={"Only on Netlix"} />
+        <TitleCards title={"Upcoming"} />
+        <TitleCards title={"Top Pics For You"} />
+      </div>
     </div>
   );
 };
